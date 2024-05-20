5a687a371eeded3c04c3a897ce59309906c96eb3
Rori <rbmasilo@gmail.com>
2024-05-20 16:40:50 +0200

    Implementation of increment and decrement function. Implement to create store

--- index.js
+++ index.js
@@ -6,4 +6,28 @@ let decrementButton = document.getElementById("decrementButton");
 // Initial state of tally app
 const initialState = { count: 0 };
 
-//
+// Increment function
+function increment(state) {
+  return { count: state.count + 1 };
+}
+
+// decrement function
+function decrement(state) {
+  return { count: state.count - 1 };
+}
+
+// Function to create store
+function createStore(initialState) {
+  let state = initialState;
+  let listeners = [];
+
+  return {
+    getState: () => state,
+    dispatch: (action) => {
+      switch (action.type) {
+        case "INCREMENT":
+          state = increment(state);
+      }
+    },
+  };
+}
