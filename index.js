f432cf78fe0402189bbdd6f25648b2515b825c22
Rori <rbmasilo@gmail.com>
2024-05-20 13:37:47 +0200

    Declaration of UI elements. Initialise state of app

---
+++ index.js
@@ -0,0 +1,9 @@
+// UI Elements
+let stateUI = document.getElementById("currentState");
+let incrementButton = document.getElementById("incrementButton");
+let decrementButton = document.getElementById("decrementButton");
+
+// Initial state of tally app
+const initialState = { count: 0 };
+
+//
